d3be70d85f6806c8b2ee568ad771a31804059990
Vihang030 <32948833+Vihang030@users.noreply.github.com>
2018-04-11 11:19:57 -0400

    First and Last name for Registration
    First and Last name is added for registration system so that we can grab and first and last name of user for the apply purpose

--- CommunityPro/Models/AccountViewModels.cs
+++ CommunityPro/Models/AccountViewModels.cs
@@ -69,6 +69,13 @@ namespace CommunityPro.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
+        //[Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
--- CommunityPro/Models/IdentityModels.cs
+++ CommunityPro/Models/IdentityModels.cs
@@ -3,12 +3,23 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
 
 namespace CommunityPro.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "First Name")]
+        //[Required(ErrorMessage ="Please enter the first name!!")]
+        [StringLength(52, ErrorMessage = "First Name can not be longer than 52 character!!")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        //[Required(ErrorMessage ="Please enter the Last name!!")]
+        [StringLength(52, ErrorMessage = "Last Name can not be longer than 52 character!!")]
+        public string LastName { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
